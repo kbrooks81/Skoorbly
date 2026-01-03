dedf8f222042120ddaca15af8cd250c1db545d1a
Kris Brooks <kbrooks8193@gmail.com>
2026-01-03 12:17:43 -0500

    set up basic folder structure

---
+++ app/(modals)/addEntry.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const AddEntry = () => {
+  return (
+    <View>
+      <Text>AddEntry</Text>
+    </View>
+  )
+}
+
+export default AddEntry
